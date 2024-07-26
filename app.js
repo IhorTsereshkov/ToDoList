f09d74e1e513edeca8ac5e6e9c1eb59e4c9d2305
IhorTsereshkov <a3415687@gmail.com>
2024-07-26 18:47:49 +0300

    revised to-do list

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const textNone = formTask.querySelector(".textnone");
 const boxMain = document.querySelector(".main");
 const modal = document.querySelector(".modal");
 const buttonModal = document.querySelector(".modal_button");
+const newDiv = document.querySelector(".newDiv");
 const arr = [];
 
 button.addEventListener("click", (event) => {
@@ -24,7 +25,9 @@ button.addEventListener("click", (event) => {
   };
   arr.push(objInf);
   boxCreate(arr);
-  modal.classList.remove("none");
+  textTask.value = "";
+  dateTask.value = "";
+  selectTask.value = "Выберите сложность задачи";
 });
 
 textTask.addEventListener("click", () => {
@@ -41,13 +44,64 @@ modal.addEventListener("click", (event) => {
 });
 
 const boxCreate = (arr) => {
+  newDiv.innerHTML = "";
   arr.forEach((item) => {
     const newBox = document.createElement("div");
     newBox.classList.add("newBox");
-    newBox.innerHTML = `
-        <h2>${item.textTask}</h2>
-        <h3>${item.dateTask}</h3>
-        <h3>${item.selectTask}</h3>`;
-    boxMain.appendChild(newBox);
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("checkbox");
+    const h2 = document.createElement("h2");
+    h2.textContent = item.textTask;
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
+    deleteButton.textContent = "Удалить";
+    newBox.appendChild(checkbox);
+    newBox.appendChild(h2);
+    newBox.appendChild(deleteButton);
+    newDiv.appendChild(newBox);
+
+    checkbox.addEventListener("click", () => {
+      if (checkbox.checked) {
+        newBox.style.backgroundColor = "green";
+        deleteButton.style.backgroundColor = "green";
+      } else {
+        newBox.style.backgroundColor = "";
+        deleteButton.style.backgroundColor = "";
+      }
+    });
+
+    deleteButton.addEventListener("click", () => {
+      newBox.remove();
+    });
   });
 };
+
+// let a;
+// let b;
+
+// function foo() {
+//   let b;
+//   b = 10;
+//   a = 15;
+//   console.log(b);
+// }
+
+// foo();
+
+// console.log(a);
+// console.log(b);
+
+// function makeCounter() {
+//   let count = 0;
+
+//   return function () {
+//     return count++; // есть доступ к внешней переменной "count"
+//   };
+// }
+
+// let counter = makeCounter();
+
+// alert(counter); // 0
+// alert(counter()); // 1
+// alert(counter()); // 2
